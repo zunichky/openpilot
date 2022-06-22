ecca675c85428c17b973061b70b94744713b81d1
Shane Smiskol <shane@smiskol.com>
2022-06-21 22:45:04 -0700

    just try this out

--- selfdrive/debug/print_docs_diff.py
+++ selfdrive/debug/print_docs_diff.py
@@ -1,19 +1,12 @@
 print("Start of print_docs_diff")
-# import os
-# from common.basedir import BASEDIR
-# from selfdrive.car import docs as new_docs
-from selfdrive.car.docs import CARS_MD_OUT
-import re
+import os
 import pickle
 
-# # TODO: if we can easily parse the stars from the generated CARS.md file, do that instead
-# os.environ['PYTHONPATH'] = os.path.join(BASEDIR, "openpilot_pr_base")
-# print(os.environ['PYTHONPATH'])
-
+from common.basedir import BASEDIR
 from selfdrive.car.docs import get_all_car_info
 from selfdrive.car.docs_definitions import Column
 
-print('All car info:')
+# print('All car info:')
 # print(old_docs.get_all_car_info())
 
 
@@ -28,22 +21,11 @@ def pretty_row(row, exclude=[Column.MAKE, Column.MODEL]):
   return {k.value: v for k, v in row.items() if k not in exclude}
 
 
-def get_old_car_info():
-  with open('/home/batman/openpilot_pr_base/docs/CARS.md', 'r') as f:
-    cars_md = f.read()
-  old_car_info = []
-  for line in cars_md.splitlines():
-    if line.startswith('|') and '</a>' in line:
-      print(line)
-      print(line.strip('|').split('|'))
+with open(os.path.join(BASEDIR, '../old_car_info'), 'rb') as f:
+  old_car_info = pickle.load(f)
 
 
-print(get_old_car_info())
-
-a = pickle.dumps(get_all_car_info())
-
-raise Exception
-old_car_info = {f'{i.make} {i.model}': i for i in old_docs.get_all_car_info()}
+old_car_info = {f'{i.make} {i.model}': i for i in old_car_info}
 new_car_info = {f'{i.make} {i.model}': i for i in get_all_car_info()}
 
 added_cars = set(new_car_info) - set(old_car_info)
