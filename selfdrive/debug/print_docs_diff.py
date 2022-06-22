870b6b25563acc41a566659a9a8efacb6898837d
Shane Smiskol <shane@smiskol.com>
2022-06-21 18:50:45 -0700

    no other tests

--- selfdrive/debug/print_docs_diff.py
+++ selfdrive/debug/print_docs_diff.py
@@ -1,9 +1,12 @@
+print("Start of print_docs_diff")
 import os
 from common.basedir import BASEDIR
+print(BASEDIR)
 
 os.environ['PYTHONPATH'] = os.path.join(BASEDIR, "openpilot_pr_base")
+print(os.environ['PYTHONPATH'])
 
 from openpilot_pr_base.selfdrive.car.docs import get_all_car_info
 
+print('All car info:')
 print(get_all_car_info())
-
